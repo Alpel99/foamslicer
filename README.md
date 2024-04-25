b38bfc82b538a9924bff9d74ebe9c2b078f97d01
pg <paul.genest@gmx.de>
2024-04-25 02:13:39 +0200

    more ideas, mesh to 0,0, speedcoeff, initial offset

--- README.md
+++ README.md
@@ -7,3 +7,22 @@ https://www.thingiverse.com/thing:94917/files
 
 gcode from online airfoil gcode generator
 https://www.diyrcwings.com/app/
+
+# ideas/todo
+## pre points ?
+* need some stuff to flip?
+* some sort of length offset between piece and real dimension
+    * shrink one side to account for it
+
+## gcode
+* set init stuff, whatever -> heat wire etc
+* standard speed: move offset
+* with speedcoeff: move both through their geometries
+    * maybe need 2 coeffs: one for top, one for bottom
+    * need to rech reverse point at the same time
+
+* other problems:
+* in what coordinates does gcode work?
+    * conversion of some sort?
+* standard offset from 0,0? -> foam not directly at start
+    * maybe add standard entering offset to certainly be in foam
--- foamslicer.py
+++ foamslicer.py
@@ -7,20 +7,29 @@
 import meshio
 import numpy as np
 import matplotlib.pyplot as plt
+np.set_printoptions(suppress=True)
+
+### INPUTS
+# which dim is linear
+i = 1
+# needs to flip xy dir?
+flip = False
 
 mesh = meshio.read("Allerion_NO_horn.stl")
 
-arr = np.array(mesh.points)
+min = np.min(mesh.points, axis=0)
+
+shifted_mesh = np.array(mesh.points) + abs(min)
+# print(shifted_mesh)
 
 maxmin = np.zeros((2,3))
-maxmin[0] = np.max(arr, axis=0)
-maxmin[1] = np.min(arr, axis=0)
-
-# print("new\n", maxmin)
+maxmin[0] = np.max(shifted_mesh, axis=0)
+maxmin[1] = np.min(shifted_mesh, axis=0)
+# print(maxmin)
 
 def getExtremePoints(val, idx):
     data = []
-    for p in mesh.points:
+    for p in shifted_mesh:
         if abs(p[idx] - val) < 2.220446049250313e-16:
             pn = []
             for j in range(0, len(p)):
@@ -29,23 +38,25 @@ def getExtremePoints(val, idx):
             data.append(pn)
     return np.array(data)
 
-i = 1
+
 d1 = getExtremePoints(maxmin[0][i], i)
 d2 = getExtremePoints(maxmin[1][i], i)
+d2 = d2*1.5
+d1 = d1+[10,1]
 
-# plt.plot(x_values, y_values, 'bo')  # 'bo' means blue circles
-
-def plotPoints(d1):
-    plt.plot(d1[:, 0], d1[:, 1], 'bo-')
+def plotPoints(points, show=False):
+    plt.plot(points[:, 0], points[:, 1], 'o-')
     # p = [-3.55537148,  2.44643784]
     # plt.plot(p[0], p[1], "ro")
-    plt.show()
+    if show: plt.show()
 
-# how to generate gcode?
+# plotPoints(d1)
+# plotPoints(d2)
+# plt.show()
 
 def orderPoints(points):
     barycenter = np.mean(points, axis=0)
-    print(barycenter)
+    # print(barycenter)
     vectors = points - barycenter
     angles = np.arctan2(vectors[:, 1], vectors[:, 0])
     sorted_indices = np.argsort(angles)
@@ -53,13 +64,25 @@ def orderPoints(points):
     ordered = np.vstack([ordered, ordered[0]])
     return ordered
     
-convex = orderPoints(d1)
-# plotPoints(convex)
+#convex1
+c1 = orderPoints(d1)
+c2 = orderPoints(d2)
+plotPoints(c1)
+plotPoints(c2)
+plt.show()
 
-def getSpeedCoeff(p1, p2):
-    pass
+def getLength(points):
+    return np.sum(abs(points[1:]-points[:-1]))
+        
+# d1 x times faster than d2
+speedcoeff = getLength(c1)/getLength(c2)
+# print(speedcoeff)
 
-def getOffset(p1, p2):
-    pass
+def getOffset(points1, points2):
+    m1 = np.min(points1, axis=0)
+    m2 = np.min(points2, axis=0)
+    diff = m1-m2
+    print(diff)
 
+getOffset(c1, c2)
 
